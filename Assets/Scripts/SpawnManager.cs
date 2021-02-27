f30d669b483eda427bca6dc7abaddf2e8995c4d0
Jason Harrison <jason@harrisonis.com>
2021-02-27 14:26:42 -0800

    Adds some titling, directions, controls, and fixes a boost speed bug

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnManager : MonoBehaviour
 {
@@ -13,7 +15,9 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private Transform _top, _bottom;
     private int _roundDistance;
-
+    public PlayerManager _playerManager;
+    private float _boostSpeed = 0;
+    
     [SerializeField] private float asteroidStart, asteroidEnd, nonAsteroidStart, nonAsteroidEnd, pickupStart, pickupEnd;
     private float _timeToNextAsteroidSpawn, _timeToNextNonAsteroidSpawn, _timeToNextPickupSpawn;
     void Start()
@@ -21,6 +25,7 @@ public class SpawnManager : MonoBehaviour
         _timeToNextAsteroidSpawn = Random.Range(asteroidStart, asteroidEnd);
         _timeToNextNonAsteroidSpawn = Random.Range(nonAsteroidStart, nonAsteroidEnd);
         _timeToNextPickupSpawn = Random.Range(pickupStart, pickupEnd);
+        
     }
 
     
@@ -29,7 +34,7 @@ public class SpawnManager : MonoBehaviour
         var asteroidToSpawn = asteroids[Random.Range(0, asteroids.Count)];
         Vector2 asteroidSpawnPosition = new Vector2(gameObject.transform.position.x, Random.Range(_bottom.position.y, _top.position.y));
         var asteroid = Instantiate(asteroidToSpawn, asteroidSpawnPosition, Quaternion.identity);
-        asteroid.GetComponent<PickupMover>().moveSpeed = Random.Range(-2f, -0.5f);
+        asteroid.GetComponent<PickupMover>().moveSpeed = Random.Range(-2f, -0.5f) + _boostSpeed;
     }
     void SpawnNonAsteroid()
     {
@@ -40,7 +45,18 @@ public class SpawnManager : MonoBehaviour
         var pickupToSpawn = pickups[Random.Range(0, pickups.Count)];
         Vector2 pickupSpawnPosition = new Vector2(gameObject.transform.position.x, Random.Range(_bottom.position.y, _top.position.y));
         var pickup = Instantiate(pickupToSpawn, pickupSpawnPosition, Quaternion.identity);
-        pickup.GetComponent<PickupMover>().moveSpeed = Random.Range(-2f, -0.5f);
+        pickup.GetComponent<PickupMover>().moveSpeed = Random.Range(-2f, -0.5f) + _boostSpeed;
+        
+    }
+
+    void ActivateBoost(PlayerManager playerManager)
+    {
+        _boostSpeed = playerManager.boostSpeed;
+    }
+
+    void DeactivateBoost(PlayerManager _)
+    {
+        _boostSpeed = 0;
     }
     
     void Update()
@@ -77,5 +93,11 @@ public class SpawnManager : MonoBehaviour
                 _timeToNextPickupSpawn -= Time.deltaTime;
             }
         }
+    }
+
+    private void OnEnable()
+    { 
+        _playerManager.OnStartBoost += ActivateBoost;
+        _playerManager.OnEndBoost += DeactivateBoost;
     }
 }
